07b6d3bd70c62ad8c6e92368f1d12f0e813289cf
Rabdool <abdurrahmanabdulkabir06@gmail.com>
2023-09-20 10:34:35 +0100

    jambscore

---
+++ jambscore.js
@@ -0,0 +1,9 @@
+function jambScore(num) {
+    if (num >= 180) {
+        console.log("Eligible for BUK admission");    
+    }
+    else{
+        console.log("Not Eligible");
+    }
+}
+console.log(jambScore(180));
